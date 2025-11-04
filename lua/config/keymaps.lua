4b05d0b8f99d7a45285929f985e11bb5a75efaad
Ishan Joshi <noobscience123@gmail.com>
2025-11-04 14:33:16 +0530

    feat: keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -31,3 +31,40 @@ keymap.set("n", "<leader>t6", "6gt", { desc = "Go to tab 6" })
 keymap.set("n", "<leader>t7", "7gt", { desc = "Go to tab 7" })
 keymap.set("n", "<leader>t8", "8gt", { desc = "Go to tab 8" })
 keymap.set("n", "<leader>t9", "9gt", { desc = "Go to tab 9" })
+
+-- Smart window navigation/creation
+local function smart_split(direction)
+	local current_win = vim.fn.winnr()
+	vim.cmd("wincmd " .. direction)
+	if vim.fn.winnr() == current_win then
+		-- Didn't move, so create a new split
+		if direction == "h" then
+			vim.cmd("topleft vsplit")
+		elseif direction == "l" then
+			vim.cmd("botright vsplit")
+		elseif direction == "k" then
+			vim.cmd("topleft split")
+		elseif direction == "j" then
+			vim.cmd("botright split")
+		end
+	end
+end
+
+keymap.set("n", "<leader>wh", function()
+	smart_split("h")
+end, { desc = "Split/go left" })
+keymap.set("n", "<leader>wj", function()
+	smart_split("j")
+end, { desc = "Split/go down" })
+keymap.set("n", "<leader>wk", function()
+	smart_split("k")
+end, { desc = "Split/go up" })
+keymap.set("n", "<leader>wl", function()
+	smart_split("l")
+end, { desc = "Split/go right" })
+
+-- Window navigation with arrow keys
+keymap.set("n", "<leader>w<Left>", "<C-w>h", { desc = "Go to left window" })
+keymap.set("n", "<leader>w<Down>", "<C-w>j", { desc = "Go to window below" })
+keymap.set("n", "<leader>w<Up>", "<C-w>k", { desc = "Go to window above" })
+keymap.set("n", "<leader>w<Right>", "<C-w>l", { desc = "Go to right window" })
